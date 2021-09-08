e0237e81f33f384743ee935ac4d4d1a921ac70c7
JasonRDT <83755146+JasonRDT@users.noreply.github.com>
2021-09-08 14:43:10 +0100

    Ticket 2 - Add booking service and validate bookings.

---
+++ PDR.PatientBooking.Service.Tests/BookingServices/BookingServiceTests.cs
@@ -0,0 +1,135 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Data.DataSeed;
+using PDR.PatientBooking.Data.Models;
+using PDR.PatientBooking.Service.BookingServices;
+using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.BookingServices.Validation;
+using PDR.PatientBooking.Service.Validation;
+using System;
+using System.Linq;
+
+namespace PDR.PatientBooking.Service.Tests.BookingServices
+{
+    [TestFixture]
+    public class BookingServiceTests
+    {
+        private MockRepository _mockRepository;
+        private IFixture _fixture;
+
+        private PatientBookingContext _context;
+        private Mock<IAddBookingRequestValidator> _validator;
+
+        private BookingService _bookingService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Boilerplate
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _fixture = new Fixture();
+
+            //Prevent fixture from generating circular references
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior(1));
+
+            // Mock setup
+            _context = new PatientBookingContext(new DbContextOptionsBuilder<PatientBookingContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+            _validator = _mockRepository.Create<IAddBookingRequestValidator>();
+
+            new DatabaseSeed(_context).SeedDatabase();
+
+            // Mock default
+            SetupMockDefaults();
+
+            // Sut instantiation
+            _bookingService = new BookingService(
+                _context,
+                _validator.Object
+            );
+        }
+
+        private void SetupMockDefaults()
+        {
+            _validator.Setup(x => x.ValidateRequest(It.IsAny<AddBookingRequest>()))
+                .Returns(new PdrValidationResult(true));
+        }
+
+        [Test]
+        public void AddBooking_ValidatesRequest()
+        {
+            //arrange
+            var request = new AddBookingRequest
+            {
+                PatientId = 100,
+                DoctorId = 1,
+                StartTime = DateTime.UtcNow.AddHours(3),
+                EndTime = DateTime.UtcNow.AddHours(4)
+            };
+            
+            //act
+            _bookingService.AddBooking(request);
+
+            //assert
+            _validator.Verify(x => x.ValidateRequest(request), Times.Once);
+        }
+
+        [Test]
+        public void AddBooking_ValidatorFails_ThrowsArgumentException()
+        {
+            //arrange
+            var failedValidationResult = new PdrValidationResult(false, _fixture.Create<string>());
+
+            var request = new AddBookingRequest
+            {
+                PatientId = 100,
+                DoctorId = 1,
+                StartTime = DateTime.UtcNow.AddHours(3),
+                EndTime = DateTime.UtcNow.AddHours(4)
+            };
+
+            _validator.Setup(x => x.ValidateRequest(It.IsAny<AddBookingRequest>())).Returns(failedValidationResult);
+
+            //act
+            var exception = Assert.Throws<ArgumentException>(() => _bookingService.AddBooking(request));
+
+            //assert
+            exception.Message.Should().Be(failedValidationResult.Errors.First());
+        }
+
+        [Test]
+        public void AddBooking_AddsOrderToContextWithGeneratedId()
+        {
+            //arrange
+            var request = new AddBookingRequest
+            {
+                PatientId = 100,
+                DoctorId = 1,
+                StartTime = DateTime.UtcNow.AddHours(3),
+                EndTime = DateTime.UtcNow.AddHours(4)
+            };
+
+            var expected = new Order
+            {
+                PatientId = request.PatientId,
+                DoctorId = request.DoctorId,
+                StartTime = request.StartTime,
+                EndTime = request.EndTime
+            };
+
+            //act
+            _bookingService.AddBooking(request);
+
+            //assert
+            _context.Order.Should().ContainEquivalentOf(expected, 
+                options => options.Including(order => order.PatientId)
+                .Including(order => order.DoctorId)
+                .Including(order => order.StartTime)
+                .Including(order => order.EndTime)
+            );
+        }
+    }
+}
---
+++ PDR.PatientBooking.Service/BookingServices/Validation/AddBookingRequestValidator.cs
@@ -0,0 +1,69 @@
+﻿using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+using System;
+using System.Linq;
+
+namespace PDR.PatientBooking.Service.BookingServices.Validation
+{
+    public class AddBookingRequestValidator : IAddBookingRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+
+        public AddBookingRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
+
+        public PdrValidationResult ValidateRequest(AddBookingRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (CheckPatientExists(request, ref result))
+                return result;
+
+            if (CheckBookingNotInThePast(request, ref result))
+                return result;
+
+            if (CheckDoctorNotAlreadyBooked(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        private bool CheckPatientExists(AddBookingRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Order.Any(o => o.PatientId == request.PatientId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Specified patient does not exist");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckBookingNotInThePast(AddBookingRequest request, ref PdrValidationResult result)
+        {
+            if (request.StartTime < DateTime.UtcNow)
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Specified booking start time is in the past");
+                return true;
+            }
+            return false;
+        }
+
+        private bool CheckDoctorNotAlreadyBooked(AddBookingRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Order.Any(o => o.DoctorId == request.DoctorId && o.StartTime >= request.StartTime && o.EndTime <= request.EndTime))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Doctor already booked between specified start and end times");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
